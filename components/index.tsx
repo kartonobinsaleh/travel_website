0f59b129a26f0bf5ff240200449f7bad5d9fe9a1
tondeveloperr <kartono.saleh@gmail.com>
2024-06-26 10:24:24 +0700

    add components

---
+++ components/index.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Camp from "./Camp";
+import Guide from "./Guide";
+import Features from "./Features";
+import GetApp from "./GetApp";
+
+export { Navbar, Footer, Hero, Camp, Guide, Features, GetApp };
